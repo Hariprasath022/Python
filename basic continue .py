3ed8f32dd843845a5bee3067c31474d97bf4fd2f
Hariprasath022 <91771808+Hariprasath022@users.noreply.github.com>
2022-01-12 20:13:02 +0530

    Update basic continue .py

--- basic continue .py
+++ basic continue .py
@@ -480,5 +480,73 @@ OUTPUT :
 55
 55
 
+tom = ['song','facts','fun']
+for n in range(3):
+    print(n, tom[n])
+OUTPUT :
+0 song
+1 facts
+2 fun
 
+fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
+for n in range(len(fruits)):
+    print(n, fruits[n])
+
+OUTPUT :
+0 apple
+1 pear
+2 apricot
+3 cherry
+4 peach
+######################
+he RGB Color Model
+import image
+
+p = image.Pixel(45, 76, 200)
+print(p.getRed())
+p.setRed(66)
+print(p.getRed())
+p.setBlue(p.getGreen())
+print(p.getGreen(), p.getBlue())
+
+OUTPUT :
+45
+66
+76 76
+
+import image
+img = image.Image("luther.jpg")
+
+print(img.getWidth())
+print(img.getHeight())
+
+p = img.getPixel(45, 55)
+print(p.getRed(), p.getGreen(), p.getBlue())
+OUTPUT ;
+400
+244
+165 161 158
+
+#IMAGE PROCCESSING - NEGATIVE IMAGE 
+import image
+
+img = image.Image("luther.jpg")
+win = image.ImageWin(img.getWidth(), img.getHeight())
+img.draw(win)
+img.setDelay(1,15)   # setDelay(0) turns off animation
+
+for row in range(img.getHeight()):
+    for col in range(img.getWidth()):
+        p = img.getPixel(col, row)
+
+        newred = 255 - p.getRed()
+        newgreen = 255 - p.getGreen()
+        newblue = 255 - p.getBlue()
+
+        newpixel = image.Pixel(newred, newgreen, newblue)
+
+        img.setPixel(col, row, newpixel)
+
+img.draw(win)
+win.exitonclick()
 
