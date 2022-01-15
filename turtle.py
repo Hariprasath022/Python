d4950fb5fb60dbbcfb049b78d92fd7bbaa8b4963
Hariprasath022 <91771808+Hariprasath022@users.noreply.github.com>
2022-01-15 12:52:52 +0530

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -246,3 +246,33 @@ result = prob * 5
 print(result)
 OUTPUT:
 0.195467714275
+
+#******************** Turtles and Conditionals***********************************week 3
+
+import turtle
+wn = turtle.Screen()
+
+amy = turtle.Turtle()
+amy.pencolor("Pink")
+amy.right(170)
+
+colors = ["Purple", "Yellow", "Orange", "Pink", "Orange", "Yellow", "Purple", "Orange", "Pink", "Pink", "Orange", "Yellow", "Purple", "Orange", "Purple", "Yellow", "Orange", "Pink", "Orange", "Purple", "Purple", "Yellow", "Orange", "Pink", "Orange", "Yellow", "Purple", "Yellow"]
+
+
+for color in colors:
+    if amy.pencolor() == "Purple":
+        amy.forward(50)
+        amy.right(59)
+    elif amy.pencolor() == "Yellow":
+        amy.forward(65)
+        amy.left(98)
+    elif amy.pencolor() == "Orange":
+        amy.forward(30)
+        amy.left(60)
+    elif amy.pencolor() == "Pink":
+        amy.forward(50)
+        amy.right(57)
+
+    amy.pencolor(color)
+
+
